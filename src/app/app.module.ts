dc7f4f8fde8f4778187ab545b28f979c29899f81
Tutti <natijago@gmail.com>
2022-05-27 15:50:19 -0500

    Commit de modulo de autenticacion finalizado, incluye 5 pantallas, signin, signup, newpassword, mailsend & myaccount

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MailsendComponent } from './modules/authentication/mailsend/mailsend.component';
 import { FormsModule } from '@angular/forms';
+import { SignupComponent } from './modules/authentication/signup/signup.component';
+import { SigninComponent } from './modules/authentication/signin/signin.component';
+import { NewpassComponent } from './modules/authentication/newpass/newpass.component';
+import { MyaccountComponent } from './modules/authentication/myaccount/myaccount.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MailsendComponent
+    MailsendComponent,
+    SignupComponent,
+    SigninComponent,
+    NewpassComponent,
+    MyaccountComponent
   ],
   imports: [
     BrowserModule,

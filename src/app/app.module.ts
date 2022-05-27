6ac335dea80f5b817b6c1a8c7c3ffb50bd2b3817
Tutti <natijago@gmail.com>
2022-05-27 14:39:46 -0500

    Commit para la pantalla de Email Send

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MailsendComponent } from './modules/authentication/mailsend/mailsend.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MailsendComponent } from './modules/authentication/mailsend/mailsend.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
